099fceafde6929053cc542403823f127d5e1ffcc
mtkastner <kastner@galt.de>
2022-09-29 11:29:57 +0200

    fixed absolute asset urls

--- config/env/production-config.js
+++ config/env/production-config.js
@@ -3,7 +3,7 @@ const baseConfig = require('./base-config');
 module.exports = Object.assign(baseConfig, {
   logLevel: 'info',
   //assetUri: 'https://fmh-finanzberatung.github.io/fmh-simple-site',
-  //assetUri: 'https://static.fmh.de/sites/www.fmh.de',
-  //baseUri: 'https://www.fmh.de',
+  assetUri: 'https://www.der-markt.com',
+  baseUri: 'https://www.der-markt.com',
   hashedAssetUris: true,
 });
